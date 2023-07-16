219d6b2f229ca9c24c9e402eb85fba97be3af183
bikkubs97 <bikku4444@gmail.com>
2023-07-16 15:30:44 +0530

    new

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,46 +6,44 @@ export default function Welcome() {
   const [message, setMessage] = useState('');
 
   function handleSignUp() {
-    if (username && password) {
-      setMessage('Please Wait...!');
-      fetch('https://notes-server-2anm.onrender.com/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          name: username,
-          password: password,
-        }),
-      
-      })
-      
-        .then((response) => {
-          if (response.status === 201) {
-            console.log('success');
-            setMessage('Success! Please Login Now!');
-          }
-          return response.text();
-        })
-
-        .then(() => {
-          setTimeout(() => {
-            window.location.href = '/login';
-          }, 1000);
-        })
-        .then((responseText) => {
-          console.log(responseText);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    } else {
-      window.alert('You must provide a valid Name and Password');
+    if (username.length < 5 || password.length < 8) {
+      setMessage('Username must be at least 5 characters long, and password must be at least 8 characters long.');
+      return;
     }
+
+    setMessage('Please Wait...!');
+    fetch('https://notes-server-2anm.onrender.com/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: username,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          console.log('success');
+          setMessage('Success! Please Login Now!');
+        }
+        return response.text();
+      })
+      .then(() => {
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 1000);
+      })
+      .then((responseText) => {
+        console.log(responseText);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   return (
     <div className="signup">
       <div className="login-form">
-        <div id="msg">{message}</div>
+        {message && <div id="msg">{message}</div>}
         <h1>Notes</h1>
         <p>Welcome to Notes, create and save your notes!</p>
         <h2>Create Your Account</h2>
@@ -54,8 +52,12 @@ export default function Welcome() {
         <input
           id="user"
           required
+          minLength={5}
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            setMessage(''); // Reset message when user starts typing
+          }}
         />
         <br />
         <div className="password">
@@ -64,9 +66,13 @@ export default function Welcome() {
           <input
             id="password"
             type="password"
+            minLength={8}
             required
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setMessage(''); // Reset message when user starts typing
+            }}
           />
           <br />
         </div>
