ee97f42987bf9ddac052486d54b9378df1f68fa5
bikkubs97 <bikku4444@gmail.com>
2023-08-10 16:24:12 +0530

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import notesReducer from '../reducer.js';
-import Account from './Account.jsx';
-import Login from './Login.jsx';
-import SignUp from './SignUp.jsx';
-import New from './New.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import notesReducer from "../reducer.js";
+import Account from "./Account.jsx";
+import Login from "./Login.jsx";
+import SignUp from "./SignUp.jsx";
+import New from "./New.jsx";
 
 const store = createStore(notesReducer);
 
@@ -15,9 +15,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<SignUp />} />
           <Route path="/account/:username" element={<Account />} />
           <Route path="/new/:username" element={<New />} />
+          <Route path="/" element={<SignUp />} />
         </Routes>
       </Router>
     </Provider>
